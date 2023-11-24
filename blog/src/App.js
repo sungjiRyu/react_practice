ab6b02d32b85e63f93fe4d3916a9d0c0a3c93909
sungjiRyu <116089824+sungjiRyu@users.noreply.github.com>
2023-11-25 00:08:11 +0900

    컴포넌트 만들어보기

--- blog/src/App.js
+++ blog/src/App.js
@@ -58,9 +58,44 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-       <img></img>
+
+      {/* 컴포넌트 */}
+      {/* <Modal></Modal> */}
+      <Modal/> 
+      <Com></Com>
     </div>
   );
+}
+
+// 컴포넌트 만들기(html 을 축약할때)
+function Modal(){
+  return (
+    <div className ="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
+}
+
+// 컴포넌트는 언제 쓰는가
+// 1. 반복적인 html 이 등장할때
+// 2. 큰 페이지들을 컨포넌트로 만들어 씀
+// 3. 자주 변경되는 것들
+
+// 컴포넌트의 단점
+// state 가져다쓸때 문제생김
+// A함수의 변수는 B함수에서 가져다 쓸 수 없음
+
+// 컴포넌트 만들기 (연습)
+let Com = () => {
+  return(
+    <>
+    <div>
+      <h4>컴포넌트 만들어보기</h4>
+    </div>
+    </>
+  )
 }
 
 export default App;
